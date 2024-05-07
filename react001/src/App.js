5c98adc016d298d82a1e02859e967487c3b7427b
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-07 15:45:06 +0100

    add new form to create render

--- react001/src/App.js
+++ react001/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 function App() {
     return(
         <header>
-            <h1>Hello World Iam Here!</h1>
+            <h1>Hello ReactJs Iam Here!</h1>
         </header>
         
     )
--- react001/src/index.js
+++ react001/src/index.js
@@ -1,8 +1,11 @@
 
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
-ReactDOM.render(<App></App>,document.querySelector('#root'));
+ReactDOM.createRoot(document.querySelector("#root")).render(<App />)
+
+//ReactDOM.render(<App></App>,document.querySelector('#root'));
 
 /*const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
