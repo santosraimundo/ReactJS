79dea620d75583811590f09626290696a63c1220
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-08 17:50:45 +0100

    add new components

--- react002/src/App.js
+++ react002/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Texto  from "./components/Texto";
 
 function App(){
     return(
         <header>
             <h1>Vamos colocar componentes dentro de componentes</h1>
+            <Texto />
         </header>
+        
+        
     )
 }
 export default App;
---
+++ react002/src/components/Texto.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Texto(){
+    return(
+        <main>
+            <aside>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nihil veniam iusto expedita aliquid ratione deleniti voluptatibus labore quaerat perferendis dicta cum harum. Deleniti quisquam aspernatur vero nostrum perferendis a?</p>
+            </aside>
+        </main>
+    )
+}
+export default Texto;
