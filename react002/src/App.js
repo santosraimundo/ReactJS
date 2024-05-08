e9e1733cedc6f7fbc0b35fba14844e114bcb25ee
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-08 17:45:53 +0100

    Add new project

---
+++ react002/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App(){
+    return(
+        <header>
+            <h1>Vamos colocar componentes dentro de componentes</h1>
+        </header>
+    )
+}
+export default App;
---
+++ react002/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import App from'./App'
+
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
+//ReactDOM.createRoot(document.querySelector('#root')).render('Cheguei');
+/*const root = document.querySelector('#root');
+ReactDOM.createRoot(root).render('Vamos começar');*/
