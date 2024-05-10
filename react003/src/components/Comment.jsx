3e9c8aef66cc633a615ecb543b07a06f84c0fbac
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-10 15:56:22 +0100

    add style to my site

---
+++ react003/src/components/Comment.jsx
@@ -0,0 +1,14 @@
+import React  from "react";
+import "./Comment.css";
+function Comment(){
+    return(
+        <>
+            <section>
+                <h1>Título</h1>
+                <h3>Título2</h3>
+                <h6>Autor: Santos Mateus Raimundo</h6>
+            </section>
+        </>
+    )
+}
+export default Comment;
