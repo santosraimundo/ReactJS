715886cbea51bcc23481e0596e43c56d79f35093
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-10 16:05:31 +0100

    features to my site

--- react003/src/components/Comment.jsx
+++ react003/src/components/Comment.jsx
@@ -3,10 +3,10 @@ import "./Comment.css";
 function Comment(){
     return(
         <>
-            <section>
-                <h1>Título</h1>
-                <h3>Título2</h3>
-                <h6>Autor: Santos Mateus Raimundo</h6>
+            <section className="comment">
+                <h1 className="title">Título</h1>
+                <h3 className="title2">Título2</h3>
+                <h6 className="author">Autor: Santos Mateus Raimundo</h6>
             </section>
         </>
     )
