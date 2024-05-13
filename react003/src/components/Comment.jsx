6c93f10d958333c00a2d2b526053709b0b8ea1f0
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-13 13:43:38 +0100

    add new features

--- react003/src/components/Comment.jsx
+++ react003/src/components/Comment.jsx
@@ -1,12 +1,19 @@
 import React  from "react";
 import "./Comment.css";
 function Comment(props){
+    let number = 125469;
+    function Maiuculas(texto){
+        return(
+            texto.toUpperCase()
+        )
+    }
     return(
         <>
             <section className="comment">
-                <h1 className="title">{props.title}</h1>
+                <h1 className="title">{Maiuculas(props.title)}</h1>
                 <h3 className="title2">{props.head}</h3>
                 <h6 className="author">{props.author}</h6>
+                <h4>{number}</h4>
             </section>
         </>
     )
