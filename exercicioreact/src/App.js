e39df986e09b2ae5ef46297bec82542e9d485d16
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-16 17:02:22 +0100

    onganize my simple site

--- exercicioreact/src/App.js
+++ exercicioreact/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import QualityReact from "./components/QualityReact.jsx"
 
 function App(){
     return(
         <>
-            <h1>Come and see how powerful ReactJs is!</h1>
+            <QualityReact />
         </>
     )
 }
---
+++ exercicioreact/src/components/QualityReact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function QualityReact(){
+    return(
+        <div>
+        <header class="head">
+            <h1>Programing with ReactJS is a Life</h1>
+        </header>
+        <main class="container">
+            <aside>
+                <p>Now we leaning React to developer our site and to grow up with my knowlodge. </p>
+            </aside>
+        </main>
+        </div>
+    )
+}
+export default QualityReact;
