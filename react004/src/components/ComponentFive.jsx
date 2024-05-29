24c3c08e61ccc89b9fd3969e0be24603068bf38d
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-05-29 15:11:42 +0100

    add new style

--- react004/src/components/ComponentFive.jsx
+++ react004/src/components/ComponentFive.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Clientes from "../cliente/dados_clientes";
 
+        function cliente(){
+          return Clientes.map(colecao_cliente =>{
+            return <li key ={colecao_cliente.id_clientes}>Nome: {colecao_cliente.name} | Email: {colecao_cliente.email}</li>
+           })
+        }
 const ComponentFive = (props) =>{
     return(
         <div className="component">
-            <h5>Títul5: {props.title}</h5>
+            <h5>Título5: {props.title}</h5>
             <p>Subtítulo: {props.subtitle}</p>
+            <ul>
+                {cliente()}
+            </ul>
         </div>
 
     )
