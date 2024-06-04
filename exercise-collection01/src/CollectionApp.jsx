0b1e0c900769b0639b8d292e389216739ffac524
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-06-04 18:48:48 +0100

    put code

--- exercise-collection01/src/CollectionApp.jsx
+++ exercise-collection01/src/CollectionApp.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function CollectionApp(props){
     return(
-        <section className="collection">
-            <h1>Título1: {props.title}</h1>
-            <ul></ul>
-        </section>
+        <div className="container">
+            <section className="collection">
+                <h1>Título1: {props.title}</h1>
+                <ul></ul>
+            </section>
+        </div>
     )
 }
 export default CollectionApp;
