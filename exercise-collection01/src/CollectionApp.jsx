b3cd422e2872ab2a2ebb0fbc4c913412d696fcb5
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-06-04 19:16:47 +0100

    Update CollectionApp.jsx

--- exercise-collection01/src/CollectionApp.jsx
+++ exercise-collection01/src/CollectionApp.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import course_collection from "./course-collection";
+
+const course = () =>{
+    return course_collection.map(get_course =>{
+        return(
+            <li key={get_course.id_course}>Nome: {get_course.course_name}</li>
+        )
+    })
+}
+
 
 function CollectionApp(props){
     return(
         <div className="container">
             <section className="collection">
                 <h1>Título1: {props.title}</h1>
-                <ul></ul>
+                <ul>{course()}</ul>
             </section>
-            
+
         </div>
     )
 }
