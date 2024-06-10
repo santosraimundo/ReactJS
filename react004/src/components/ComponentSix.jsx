7a02f0ff64bea6be518f49080b54df9fed46213a
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-06-10 17:52:45 +0100

    add new function to components

--- react004/src/components/ComponentSix.jsx
+++ react004/src/components/ComponentSix.jsx
@@ -4,7 +4,8 @@ import React from "react";
 const ComponentSix = (props)=>{
     return(
         <div className="component">
-            <h6> Titulo6: {}</h6>
+            <h6> Titulo6: {props.title}</h6>
+            <p>Subtitulo: {props.subtitle}</p>
         </div>
     )
 }
