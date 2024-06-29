a66293ac6cd4b52654af067a1b4704c983b17414
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-06-29 16:31:52 +0100

    Update ComponentSeven.jsx

--- react004/src/components/ComponentSeven.jsx
+++ react004/src/components/ComponentSeven.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 function ComponentSeven(props){
+    const comparar = ()=>{
+        if(props.valor >=10){
+            return <p>O valor é igual à 10 ou maior.</p>
+        }else{
+            return <p>O valor é inferior que 10</p>
+        }
+    }
     return(
         <div className="component">
             <h2>Título: {props.title}</h2>
             <p>Subtítulo: {props.suntitle}</p>
+            {comparar()}
         </div>
     )
 }
