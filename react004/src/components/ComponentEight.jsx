1a576421f83a77cf7e6c3e9378ad7f6e7b3cade4
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-07-14 06:28:06 +0100

    Update ComponentEight.jsx

--- react004/src/components/ComponentEight.jsx
+++ react004/src/components/ComponentEight.jsx
@@ -4,7 +4,7 @@ import CondicaoIf from "./CondicaoIf";
 export default function(props){
     return(
         <div className="component">
-             <h5>Título5: {props.title}</h5>
+             <h4>Título5: {props.title}</h4>
              <p>Subtítulo: {props.subtitle}</p>
              <p>
                 Nome: {props.valor}
