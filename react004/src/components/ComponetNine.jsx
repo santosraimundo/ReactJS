4e17d5f3b2e34d50d0d1629281ac3b81f0c15daa
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-07 17:48:02 +0100

    Update ComponetNine.jsx

--- react004/src/components/ComponetNine.jsx
+++ react004/src/components/ComponetNine.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 function ComponentNine(props){
+    const estilo ={
+        backgroundColor: "green",
+        padding: "10px",
+        color: "white"
+       
+    }
     return(
         <div className="component">
             <h1>Título: {props.title}</h1>
             <p>Subtítulo:{props.subtitle}</p>
+            <p style={{backgroundColor:"blue", padding:"20px"}}>Vamos começar a desenvolver os sites responsivo</p>
+            <p style={estilo}>Ninguém vai nos parar</p>
 
         </div>
     )
