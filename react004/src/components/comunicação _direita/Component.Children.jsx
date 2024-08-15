5adff37e1fb0cbb96f25823c619abd03048298f8
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-15 18:49:15 +0100

    add features to component

---
+++ react004/src/components/comunicação _direita/Component.Children.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComponentChildren(props){
+    return(
+        <div className="component">
+            <p className="title">{}</p>
+            <p>Filho:</p>
+        </div>
+    )
+}
+export default ComponentChildren;
