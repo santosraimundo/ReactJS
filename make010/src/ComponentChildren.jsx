19ec80fad4e01a717aa69564e66174f05f3a23aa
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-16 07:07:43 +0100

    add new function

---
+++ make010/src/ComponentChildren.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComponentChildren = (props)=>{
+    return(
+        <div className="component">
+            <h2 className="title">Título: {}</h2>
+            <p>Filho</p>
+        </div>
+    )
+}
+export default ComponentChildren;
