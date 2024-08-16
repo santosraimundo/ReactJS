a8ebcd654a73c3cadab90d6745bdc059bb6d1815
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-16 10:58:05 +0100

    Update ComponentFather.jsx

--- make010/src/ComponentFather.jsx
+++ make010/src/ComponentFather.jsx
@@ -6,7 +6,7 @@ function ComponentFather(props){
     return(
         <div className="father">
             <h1 className="title">Título: {props.title}</h1>
-            <p>Pai:</p>
+            
             <ComponentChildren title="Component Children" funcao="Técnico de Hardware" departamento="ITDATA-BCX"> Nome: Santos Raimundo</ComponentChildren>
             <ComponentChildren title="Component Children" funcao="Técnico Sénior de Hardware" departamento="ITDATA-BCX">Nome: Ismael Cristovão</ComponentChildren>
             <ComponentChildren title="Component Children" funcao="Especilista de Hardware" departamento="ITDATA-BCX">Nome: José Renato</ComponentChildren>
