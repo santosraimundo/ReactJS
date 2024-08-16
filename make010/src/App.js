21b6d0bcef5970c4ece4fda5d4c3b4128906e20a
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-16 07:28:18 +0100

    Update App.js

--- make010/src/App.js
+++ make010/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import ComponentFather from "./ComponentFather";
 
 function App(){
+    <>
     <ComponentFather title= "Component Father"></ComponentFather>
+    </>
 }
 export default App;
