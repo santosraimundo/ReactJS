8a6aa1fe7bc7a3cd6624d67deaad588dfb9152f9
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-26 15:33:13 +0100

    Update index.js

--- revisao-de-react/src/index.js
+++ revisao-de-react/src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom/client";
 import App from "./App";
 
 const root = document.querySelector("#root");
 
-ReactDOM.createRoot=(root).render(<App></App>)   
+ReactDom.createRoot=(root).render(<App></App>)   
