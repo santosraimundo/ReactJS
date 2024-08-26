5b61513f715b95ed5b88ed5ea1cb0dcde4cde118
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-26 16:24:38 +0100

    Update ComponenetChildren.jsx

--- revisao-de-react/src/ComponenetChildren.jsx
+++ revisao-de-react/src/ComponenetChildren.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+    const Condicao=(props)=>{
+        if(props.condicao){
+            return props.children
+        }else{
+            return false
+        }
+    }
     const estilo={
         backgroundColor:"white",
         height: "300px",
@@ -11,6 +18,7 @@ const ComponentChildren = (props)=>{
     return(
         <div style={estilo}>
             <h2>Título: {props.title}</h2>
+            <p> Subtítulo: {Condicao()}</p>
         </div>
     )
 
