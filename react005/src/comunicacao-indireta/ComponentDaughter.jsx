1b21eaa30156dae3981b1f4e2e39b3c38d86114a
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-29 08:20:03 +0100

    update

--- react005/src/comunicacao-indireta/ComponentDaughter.jsx
+++ react005/src/comunicacao-indireta/ComponentDaughter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ComponentDaughter = (props)=>{
     return(
-        <div style={{backgroundColor: "blue", color: "white", padding: "10px", height:"300px"}}>
+        <div style={{backgroundColor: "blue", color: "white", padding: "10px", height:"300px", margin:"15px"}}>
             <p>Título: {props.title}</p>
             <button onClick={props.clicar}>Clicar</button>
         </div>
