5e0fbf3ddd02ce7557f8b8960ab0b6f8c2599728
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-29 08:04:39 +0100

    update my site

--- react005/src/comunicacao-indireta/ComponentDaughter.jsx
+++ react005/src/comunicacao-indireta/ComponentDaughter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ComponentDaughter = (props)=>{
     return(
-        <div>
+        <div style={{backgroundColor: "blue", color: "whi"}}>
             <p>Título: {props.title}</p>
             <button onClick={props.clicar}>Clicar</button>
         </div>
--- react005/src/comunicacao-indireta/ComponentMother.jsx
+++ react005/src/comunicacao-indireta/ComponentMother.jsx
@@ -4,9 +4,15 @@ import ComponentDaughter from "./ComponentDaughter";
 function ComponentMother(props){
     function Clicou(){
         window.alert("O botão foi acionado pelo Componente Filha")
+
+        const estilo ={
+            backgroundColor:"gray",
+            margin: "5px"
+
+        }
     }
     return(
-        <div>
+        <div >
             <h1 className="title"> Título: {props.title}</h1>
             <ComponentDaughter title="A comunicação indireta é quando é usado a propridade do filho para o pai" clicar={Clicou}></ComponentDaughter>
         </div>
