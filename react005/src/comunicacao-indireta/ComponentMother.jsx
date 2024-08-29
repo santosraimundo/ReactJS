5c2133f0fa1061ff11d8cfe4d2cc0d7d0182011e
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-29 08:08:14 +0100

    update feactures

--- react005/src/comunicacao-indireta/ComponentMother.jsx
+++ react005/src/comunicacao-indireta/ComponentMother.jsx
@@ -3,7 +3,7 @@ import ComponentDaughter from "./ComponentDaughter";
 
 function ComponentMother(props){
     function Clicou(){
-        window.alert("O botão foi acionado pelo Componente Filha")
+        window.alert("O botão foi acionado pelo Componente Filha.")
 
         const estilo ={
             backgroundColor:"gray",
@@ -14,7 +14,7 @@ function ComponentMother(props){
     return(
         <div >
             <h1 className="title"> Título: {props.title}</h1>
-            <ComponentDaughter title="A comunicação indireta é quando é usado a propridade do filho para o pai" clicar={Clicou}></ComponentDaughter>
+            <ComponentDaughter title="A comunicação indireta é quando é usado a propridade do filho para o pai." clicar={Clicou}></ComponentDaughter>
         </div>
     )
 }
