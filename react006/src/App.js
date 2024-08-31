05f488e95adadfc3f21265358efdd41564dffa34
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-08-31 14:52:08 +0100

    update my site

---
+++ react006/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ComponentA from "./comunicacao-inderect-v2/ComponentA";
+
+function App(){
+    return(
+        <>
+        
+    </>
+    )
+}
+export default App;
---
+++ react006/src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from "react-dom/client";
+import App from "./App";
+
+const root = document.querySelector("#root");
+
+ReactDom.createRoot(root).render(<App></App>)
