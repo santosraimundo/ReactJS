417d00f3483b7dc7a49efae01cd78f847f3a077b
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-09-03 17:02:55 +0100

    update

--- revisao-de-react/src/ComponenetChildren.jsx
+++ revisao-de-react/src/ComponenetChildren.jsx
@@ -10,9 +10,16 @@ import React from "react";
     }
 
 const ComponentChildren = (props)=>{
+    function validar(){
+        return(
+                props.mostrar("vamos Começar",  "Name: What is your name?")
+        )
+    }
     return(
         <div style={estilo}>
             <h2>Título: {props.title}</h2>
+            <button onClick={validar}>Clicar</button>
+            
             
         </div>
     )
--- revisao-de-react/src/ComponentFather.jsx
+++ revisao-de-react/src/ComponentFather.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ComponentChildren from "./ComponenetChildren";
-import { Component } from "react";
 
 function ComponentFather(props){
+    const apresenta =(valor, name)=>{
+            
+            window.alert(valor);
+            window.prompt(name);
+            
+
+        
+    }
     return(
         <div style={{backgroundColor:"gray", height: "1060px"}}>
             <h1>Título: {props.title}</h1>
             <p>Este é o campo do componente Pai</p>
-            <ComponentChildren title="Componente Filho" condicao></ComponentChildren>
+            <ComponentChildren title="Componente Filho" mostrar={apresenta}></ComponentChildren>
             <ComponentChildren title="Componente Filho"></ComponentChildren>
             <ComponentChildren title="Componente Filho"></ComponentChildren>
         </div>
