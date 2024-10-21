cfb82f8f2703c75074bcf087efdf972ebed07f10
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-10-21 18:56:23 +0100

    increase more function

--- react008/src/AplicaConhecimento.jsx
+++ react008/src/AplicaConhecimento.jsx
@@ -3,7 +3,9 @@ import React, {useState} from "react";
 export default function AplicaConhecimento(){
     const [pessoa, setNome] = useState({nome: "Santos", apelido: "Raimundo", profissao:"Programador Web", idade: 32});
 
-
+    function change(){
+        setNome(pessoa="Baruk")
+    }
         
 
     return(
@@ -15,6 +17,7 @@ export default function AplicaConhecimento(){
             <ul><li>{pessoa.apelido}</li></ul>
             <ul><li>{pessoa.profissao}</li></ul>
             <ul><li>{pessoa.idade}</li></ul>
+            <ul><li>{}</li></ul>
 
         </section>
     )
