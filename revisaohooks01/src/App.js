6b507c1831edcb146faf5a2a5ba9fb69db4a69af
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-11-13 19:06:23 +0100

    Update App.js

--- revisaohooks01/src/App.js
+++ revisaohooks01/src/App.js
@@ -6,12 +6,16 @@ function App() {
         setValor(valor + 1);
         
     }
+    function decremento(){
+        setValor(valor - 1)
+    }
     return(
         <>
             <h1>Welcome to my React Hooks 2024</h1>
             <form action="#">
                  <p>Contar = {valor}</p>
                  <button onClick={incremento}>Incrementar</button>
+                 <button onClick={decremento}>Incrementar</button>
             </form>
         </>
     )
