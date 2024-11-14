849bddef4af8239bfe0c122b98a75a1f731db258
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-11-14 18:28:21 +0100

    Update ComponentAll.jsx

--- revsiaohooks02/src/ComponentAll.jsx
+++ revsiaohooks02/src/ComponentAll.jsx
@@ -1,8 +1,25 @@
 import React, {useState} from "react";
 
 function ComponentAll(){
+
+        const [nome, setNome]= useState(10);
+        function mostra(){
+            setNome(nome + 20)
+        }
+    
+
+                
+           
+
+    
+
     return(
-        <h1>Programming with React Hooks is very nice!</h1>
+        <main>
+            <h1>Programming with React Hooks is very nice!</h1>
+            <p>O valor vai de {nome}</p>
+            <button onClick={mostra}>Começa a contar</button>
+            
+        </main>
     )
 }
 export default ComponentAll;
