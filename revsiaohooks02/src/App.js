f59d0fe6cb8d0f2f7855c0f5f6466432349c47aa
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-11-14 17:54:11 +0100

    update new revision

---
+++ revsiaohooks02/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ComponentAll from "./ComponentAll";
+function App(){
+    return(
+        <>
+            <ComponentAll></ComponentAll>
+        </>
+    )
+}
+export default App;
---
+++ revsiaohooks02/src/ComponentAll.jsx
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+
+function ComponentAll(){
+    return(
+        <h1>Programming with React Hooks is very nice!</h1>
+    )
+}
+export default ComponentAll;
