0d5837cc26263be32d534cd8c308b60051004ddd
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-11-17 16:13:26 +0100

    Update App.js

--- revisaohooks03/src/App.js
+++ revisaohooks03/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 function App (){
     const [valor, setValor]= useState(0)
+    const [color, setColor]= useState("yellow");
     function acrescentar(){
         setValor(v_old => v_old +50)
         
@@ -11,13 +12,20 @@ function App (){
         setValor(old => old -30);
         
     }
+    function changeColor(){
+        setColor(seeA =>{
+            return{ ...seeA, color: "Blue"}
+        });
+    }
     return(
         <>
         <h1>Come to see the hooks state</h1>
         <h2>The right way to write React Hooks useState</h2>
         <button onClick={acrescentar}>Acrescentar</button>
         <span> Valor: {valor} </span>
-        <button onClick={retirar}>Retirar</button>
+        <button onClick={retirar}>Retirar</button><br />
+        <button onClick={changeColor}>Mostra Cor</button>
+        <h1> The color is: {color}</h1>
         </>
     )
 }
