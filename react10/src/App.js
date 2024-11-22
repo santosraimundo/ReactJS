b6b2d8ca2b8e47f3ba3a3017a27293e80b17671b
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-11-22 18:11:19 +0100

    Update App.js

--- react10/src/App.js
+++ react10/src/App.js
@@ -4,6 +4,21 @@ function App(){
     const[name, setName]= useState(() =>{
         return "Santos";
     })
+    const[dadosPessoas, setDadosPessoas]=useState({
+        Name: "Santos Raimundo", 
+        idade: 32,
+        Profissao: "DEV WEB",
+        Peso: 73,
+        Altura: 1.80
+    });
+    function inserirDados(){
+        setDadosPessoas(alterarDados=>{
+            return{...alterarDados,
+                dadosPessoas: alterarDados.Name => alterarDados
+
+            }
+        })
+    }
     function alteraNome(){
         setName(nameOld =>{
             return "Benivaldo"
@@ -15,6 +30,12 @@ function App(){
             <h1>Modificar os valores de uma string e objectos com useState.</h1>
             <h2>Nome: {name}</h2>
             <button onClick={alteraNome}>Alterar Nome</button>
+            <hr></hr>
+            <ul>
+                <li>Nome: {dadosPessoas.Name}</li>
+            </ul>
+            <button onClick={alteraNome}>Modificar Dados</button>
+
         </main>
     )
 }
