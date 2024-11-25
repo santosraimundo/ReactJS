2557d272470cec7b52db3c0b4a7a132c3f50d6c8
Santos Raimundo <114391701+santosraimundo@users.noreply.github.com>
2024-11-25 15:44:01 +0100

    Update App.js

--- revisaohooks03/src/App.js
+++ revisaohooks03/src/App.js
@@ -5,6 +5,13 @@ function App (){
     const [color, setColor]= useState(()=>{
         return "Yellow";
     });
+    const [rentaCar, setRentaCar]=useState({
+        carModelo: "Elentra",
+        marcaCar: "Huyindai",
+        carColour: "Preto",
+        paisOrigen: "Correa",
+        anoFabrico: 2024
+    })
     function acrescentar(){
         setValor(v_old => v_old +50)
         
@@ -21,6 +28,16 @@ function App (){
         });
        
     }
+    function modifyRentaCar(){
+        setRentaCar(trocar=>{
+            return{...trocar,
+                carModelo: "Suziki Expresso",
+                anoFabrico: 2025,
+                marcaCar: "Suziki",
+                paisOrigen: "Japão"
+            }
+        })
+    }
     return(
         <>
         <h1>Come to see the hooks state</h1>
@@ -30,6 +47,18 @@ function App (){
         <button onClick={retirar}>Retirar</button><br />
         <button onClick={changeColor}>Mostra Cor</button>
         <h1> The color is: {color}</h1>
+        <hr></hr>
+        <h2>Mostra o modelo do da Renta Car</h2>
+        <article>
+            <button onClick={modifyRentaCar}>Apresenta Car</button>
+            <ul>
+                <li>Modelo do Carro: {rentaCar.carModelo}</li>
+                <li>Cor do Carro: {rentaCar.carColour}</li>
+                <li>Marca do Carro: {rentaCar.marcaCar}</li>
+                <li>Pais de Origem: {rentaCar.paisOrigen}</li>
+                <li>Ano que foi Fabricado: {rentaCar.anoFabrico}</li>
+            </ul>
+        </article>
         </>
     )
 }
